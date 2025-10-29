3c326244a05fe6e3f84ff237a225d6acd2867b06
Tobias S <tobias_schindler@hotmail.de>
2025-10-29 20:31:20 +0100

    [LayoutBrowser.vue] - added SampleCamera component for browser testing

--- src/layouts/LayoutBrowser.vue
+++ src/layouts/LayoutBrowser.vue
@@ -1,35 +1,17 @@
 <template>
-  <div class="bg-neutral-950 text-neutral-200 w-screen h-screen">
+  <div class="bg-neutral-200 text-neutral-800 w-screen h-screen">
     <div class="text-2xl flex items-center justify-center text-center">
-      <h1 v-if="isMobile">
-        Um die App verwenden zu können, musst du sie auf deinen Home-Bildschrim installieren.
-      </h1>
+      <SampleCamera v-if="!isMobile" />
 
       <h1 v-else>
         Bitte verwende dein Mobiles Gerät.
       </h1>
     </div>
-
-    <div class="p-12 flex items-center justify-center">
-      <div class="">
-        <button
-          class="btn btn-primary"
-        >
-          Kamera öffnen
-        </button>
-
-        <button
-          class="btn btn-secondary"
-        >
-          Kamera stoppen
-        </button>
-      </div>
-    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import SampleCamera from '../components/SampleCamera.vue'
 
 interface Props {
   isMobile: boolean
