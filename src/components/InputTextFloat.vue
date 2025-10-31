9b4b3bf22ff0399f1b7109e63abf2263ee364dc6
Tobias S <tobias_schindler@hotmail.de>
2025-10-31 13:01:26 +0100

    [InputTextFloat.vue] - add new component

---
+++ src/components/InputTextFloat.vue
@@ -0,0 +1,31 @@
+<template>
+  <label class="floating-label">
+    <input
+      v-model="model"
+      :type="type"
+      :placeholder="labelText"
+      class="input input-md w-full"
+    >
+    <span>{{ labelText }}</span>
+  </label>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+interface Props {
+  label: string
+  required?: boolean
+  type?: 'text' | 'date'
+}
+interface Emits {
+
+}
+
+const { label, required = false, type = 'text' } = defineProps<Props>()
+defineEmits<Emits>()
+
+const model = defineModel()
+
+const labelText = computed((): string => required ? `${label}*` : label)
+</script>
