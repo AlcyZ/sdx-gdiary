866ed175c64ddbf367d70a64acdeaf9d02fc3c91
Tobias S <tobias_schindler@hotmail.de>
2025-10-31 13:02:59 +0100

    [InputPhoto.vue] - refactored using DaisyUI

--- src/components/InputPhoto.vue
+++ src/components/InputPhoto.vue
@@ -1,139 +1,130 @@
 <template>
-  <div>
-    <div class="flex justify-between">
-      <div class="my-1 relative flex-1">
-        <label
-          :for="id"
-          class="cursor-text peer-focus:cursor-default peer-autofill:-top-2 absolute left-2 -top-2 z-[1] px-2 text-xs text-slate-400 transition-all before:absolute before:top-0 before:left-0 before:z-[-1] before:block before:h-full before:w-full before:bg-transparent before:transition-all peer-placeholder-shown:top-2.5 peer-placeholder-shown:text-sm peer-required:after:text-pink-500 peer-required:after:content-['\00a0*'] peer-invalid:text-pink-500 peer-focus:-top-2 peer-focus:text-xs peer-focus:text-emerald-500 peer-invalid:peer-focus:text-pink-500 peer-disabled:cursor-not-allowed peer-disabled:text-slate-400 peer-disabled:before:bg-transparent"
-        >
-          {{ label }}
-        </label>
-      </div>
+  <div class="mt-3">
+    <div
+      class="flex items-center justify-between"
+    >
+      <span>{{ label }}</span>
 
-      <div class="space-x-1">
-        <BtnPrimaryIcon
-          v-if="camera === null"
-          @click="openCamera"
-        >
-          <IconCamera />
-        </BtnPrimaryIcon>
-        <BtnSecondary
-          v-else
-          @click="closeCamera"
-        >
-          <IconCameraOff />
-        </BtnSecondary>
-      </div>
-    </div>
-
-    <div class="my-2 space-y-1">
-      <div class="flex">
-        <label
-          v-if="cameraError"
-          class="text-xs text-red-400 flex-shrink min-w-0 break-words px-4"
-        >{{ cameraError.message }}</label>
-
-        <div v-if="camera" class="toolbar flex items-center justify-end space-x-1 ml-auto">
-          <BtnSecondary @click="toggle">
-            <IconSwitchCamera />
-          </BtnSecondary>
-
-          <BtnPrimaryIcon @click="captureImage">
+      <div class="flex items-center space-x-1">
+        <template v-if="!isCameraActive">
+          <button
+            class="btn btn-sm btn-square"
+            @click="startCamera"
+          >
+            <IconCamera />
+          </button>
+        </template>
+        <template v-else>
+          <button
+            class="btn btn-circle bg-red-400 text-base-200"
+            @click="captureImage"
+          >
             <IconCircle />
-          </BtnPrimaryIcon>
-        </div>
-      </div>
+          </button>
 
-      <div
-        ref="videoContainer"
-        class="max-w-full"
-      />
+          <button
+            class="btn btn-sm btn-square"
+            @click="toggle"
+          >
+            <IconSwitchCamera />
+          </button>
 
-      <div v-if="displayPreview" class="flex justify-between">
-        <img
-          class="max-w-1/3 rounded-xs"
-          :src="modelValue"
-          alt="plant preview"
-        >
-
-        <div class="flex flex-col space-y-1">
-          <BtnSecondary @click="$emit('update:modelValue', '')">
-            <IconX />
-          </BtnSecondary>
-        </div>
+          <button
+            class="btn btn-sm btn-square"
+            @click="stopCamera"
+          >
+            <IconCameraOff />
+          </button>
+        </template>
       </div>
     </div>
+
+    <div v-if="showPreview" class="flex justify-between mt-2">
+      <img
+        :src="modelValue"
+        alt="photo preview"
+        class="max-w-1/3 rounded-xs"
+      >
+
+      <button
+        class="btn btn-sm btn-circle ml-1"
+        @click="$emit('update:modelValue', '')"
+      >
+        <IconRemove />
+      </button>
+    </div>
+
+    <div
+      v-if="cameraError"
+      class="mb-0"
+    >
+      <span class="text-xs text-red-400">{{ cameraError.message }}</span>
+    </div>
+
+    <div
+      v-show="isCameraActive"
+      ref="cameraContainer"
+      class="mt-2"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import type { WebRTCCameraOpts } from '../modules/camera/webRTCCamera.ts'
 import {
   Camera as IconCamera,
   CameraOff as IconCameraOff,
-  Circle as IconCircle,
+  CircleDot as IconCircle,
+  X as IconRemove,
   SwitchCamera as IconSwitchCamera,
-  X as IconX,
 } from 'lucide-vue-next'
-import { computed, ref, useId } from 'vue'
+import { computed, ref } from 'vue'
 import { useWebRTCCamera } from '../composables/useWebRTCCamera.ts'
 import { isBase64String } from '../modules/db'
-import BtnPrimaryIcon from './BtnPrimaryIcon.vue'
-import BtnSecondary from './BtnSecondaryIcon.vue'
 
 interface Props {
   label: string
-  modelValue?: string
+  modelValue: string
 }
 interface Emits {
-  'update:modelValue': [img: string]
+  'update:modelValue': [value: string]
 }
 
-const { modelValue = '' } = defineProps<Props>()
-const emits = defineEmits<Emits>()
-
-const id = useId()
-const videoContainer = ref<HTMLDivElement | null>(null)
+const { modelValue } = defineProps<Props>()
+const emit = defineEmits<Emits>()
 
 const {
-  camera,
+  isCameraActive,
   cameraError,
   start,
   stop,
   toggle,
+  capture,
 } = useWebRTCCamera()
 
-const displayPreview = computed((): boolean => isBase64String(modelValue))
+const cameraContainer = ref<HTMLDivElement | null>(null)
+const showPreview = computed((): boolean => isBase64String(modelValue))
 
-async function openCamera() {
-  if (videoContainer.value === null) {
-    console.error('[InputPhoto:openCamera] - videoContainer element is null]')
+async function startCamera() {
+  if (cameraContainer.value === null) {
+    console.warn('[InputPhoto:startCamera] - Could not start camera, camera container element not found!')
     return
   }
 
-  const opts: WebRTCCameraOpts = {
-    video: {
-      classList: ['max-w-full'],
-    },
-  }
-  await start(videoContainer.value, opts)
+  await start(cameraContainer.value)
 }
 
-async function closeCamera() {
-  if (videoContainer.value !== null) {
-    videoContainer.value.innerHTML = ''
+async function stopCamera() {
+  if (cameraContainer.value !== null) {
+    cameraContainer.value.innerHTML = ''
   }
+
   await stop()
 }
 
 async function captureImage() {
-  if (camera.value === null) {
-    return
-  }
+  const imageBase64 = capture()
+  emit('update:modelValue', imageBase64)
 
-  const base64 = camera.value.captureBase64()
-  emits('update:modelValue', base64)
-
-  await closeCamera()
+  await stopCamera()
 }
 </script>
