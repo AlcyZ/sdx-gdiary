2a1d7f0c61e5a8f781511c5e6988779dd149310e
Tobias S <tobias_schindler@hotmail.de>
2025-11-01 14:01:45 +0100

    [IToast.vue] displays loading bar when using duration prop

--- src/components/IToast.vue
+++ src/components/IToast.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="toast z-1000" :class="toastClassList">
-    <div class="alert flex items-center justify-between text-base-100" :class="alertClassList">
-      <span>{{ message }}</span>
+    <div class="alert text-base-100 relative" :class="alertClassList">
+      <IconInfo />
+
+      <div>
+        <span>{{ message }}</span>
+      </div>
 
       <button
         class="btn btn-circle btn-soft btn-xs"
@@ -10,6 +14,12 @@
       >
         <IconX />
       </button>
+
+      <div v-if="showLoadingBar" class="absolute bottom-2.5 left-0 w-full flex items-center justify-center">
+        <div class="w-full">
+          <div class="w-full h-2 bg-gradient-to-r from-info to-green-400 border-b border-b-secondary/10" :style="loadingBarStyles" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,9 +27,11 @@
 <script lang="ts" setup>
 import type { ResponsiveBreakpoint, ToastPosition, ToastVariant } from '../types'
 import {
+  Info as IconInfo,
   X as IconX,
 } from 'lucide-vue-next'
 import { computed, onMounted } from 'vue'
+import { useProgressTimer } from '../composables/useProgressTimer.ts'
 
 interface Props {
   message: string
@@ -35,6 +47,11 @@ interface Emits {
 
 const { variant, position = 'te', positionMobile = 'bc', duration } = defineProps<Props>()
 const emit = defineEmits<Emits>()
+
+const { progress } = useProgressTimer(duration)
+
+const showLoadingBar = computed((): boolean => progress.value !== undefined)
+const loadingBarStyles = computed(() => progress.value !== undefined ? { width: `${progress.value}%` } : undefined)
 
 const alertClassMap: { [K in ToastVariant]: string } = {
   info: 'alert-info',
@@ -56,9 +73,10 @@ const toastClassList = computed(
     ...getPositionClasses(positionMobile),
   ],
 )
-const alertClassList = computed(() => [
-  variant !== undefined ? alertClassMap[variant] : undefined,
-])
+const alertClassList = computed(() => ({
+  [alertClassMap[variant || 'info']]: variant !== undefined,
+  'pb-7': showLoadingBar.value,
+}))
 const btnClassList = computed(() => variant !== undefined ? btnClassMap[variant] : undefined)
 
 function getPositionClasses(pos: ToastPosition, responsive?: ResponsiveBreakpoint): Array<string> {
