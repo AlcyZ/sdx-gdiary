d5ba79998b2499ca4fe27793f049c0a305d2450f
Tobias S <tobias_schindler@hotmail.de>
2025-11-03 18:51:57 +0100

    [ICollapse.vue] collapse component

---
+++ src/components/ICollapse.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="collapse" :class="collapseClass">
+    <input type="radio" :name="name">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+interface Props {
+  name: string
+  arrow?: boolean
+  plus?: boolean
+}
+interface Emits {
+
+}
+
+const { arrow = false, plus = false } = defineProps<Props>()
+defineEmits<Emits>()
+
+const collapseClass = computed(() => [
+  arrow ? 'collapse-arrow' : undefined,
+  plus ? 'collapse-plus' : undefined,
+].filter(Boolean))
+</script>
---
+++ src/components/ICollapseTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="collapse-title">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+
+}
+
+interface Emits {
+
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
