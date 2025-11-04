6e93975642d3dd11dee05b0b54fc113a86285534
Tobias S <tobias_schindler@hotmail.de>
2025-11-04 13:06:29 +0100

    [useModal.ts] - implemented `showModal` helper to display a modal on the current page

--- src/composables/useModal.ts
+++ src/composables/useModal.ts
@@ -1,3 +1,4 @@
+import type { Component, VNodeProps } from 'vue'
 import type { ShowConfirmationModalProps } from '../types'
 import { createApp, h } from 'vue'
 import IModalConfirmation from '../components/IModalConfirmation.vue'
@@ -22,7 +23,37 @@ export function useModal() {
     app.mount(container)
   }
 
+  const showModal = <P extends Record<string, any>>(
+    component: Component<P>,
+    props: P,
+  ) => {
+    const container = document.createElement('div')
+    document.body.appendChild(container)
+
+    const app = createApp({
+      render() {
+        // Merge user props with mandatory onClose
+        const modalProps = {
+          ...props,
+          onClose: () => {
+            app.unmount()
+            container.remove()
+
+            if ('onClose' in props && typeof props.onClose === 'function') {
+              props.onClose()
+            }
+          },
+        } as VNodeProps & P
+
+        return h(component, modalProps)
+      },
+    })
+
+    app.mount(container)
+  }
+
   return {
     showConfirmationModal,
+    showModal,
   }
 }
