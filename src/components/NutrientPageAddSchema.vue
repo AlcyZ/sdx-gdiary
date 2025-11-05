505f6e984a4504c9524390a04cffc4a150091862
Tobias S <tobias_schindler@hotmail.de>
2025-11-05 15:11:05 +0100

    [WateringSchemaForm.vue] - form component for watering schema

--- src/components/NutrientPageAddSchema.vue
+++ src/components/NutrientPageAddSchema.vue
@@ -7,70 +7,15 @@
       Düngerschema hinzufügen
     </ICardTitle>
 
-    <form
+    <WateringSchemaForm
+      v-model:name="name"
+      v-model:fertilizers-data="fertilizersData"
+      :error-name="errors.name"
+      :error-fertilizer-data="errors.fertilizersData"
       class="my-5"
-      @submit.prevent="saveAndNew"
-    >
-      <InputTextFloat
-        v-model="name"
-        :error="errors.name"
-        label="Name des Zuchtschemas"
-        required
-      />
-
-      <hr class="my-3 text-base-200">
-
-      <div class="flex items-center justify-between mb-2">
-        <h3 class="text-xl font-semibold">
-          Enthaltene Dünger
-        </h3>
-
-        <IBtn variant="primary" circle class="text-base-100" @click="addFertilizer">
-          <IconAdd />
-        </IBtn>
-      </div>
-
-      <div class="space-y-3">
-        <div
-          v-for="(fertilizerData, i) in fertilizersData"
-          :key="i"
-          class="flex items-center"
-        >
-          <div class="flex-1 flex items-center justify-between">
-            <select v-model="fertilizerData.fertilizer" class="select">
-              <option
-                v-for="(fertilizer, j) in sortedFertilizers"
-                :key="j"
-                :value="fertilizer"
-                :disabled="isSelected(fertilizer)"
-              >
-                {{ fertilizer.manufacturer ?? fallbackManufacturer }} - {{ fertilizer.name }}
-              </option>
-            </select>
-
-            <InputTextFloat
-              v-model="fertilizerData.value"
-              label="ML pro Liter"
-              type="number"
-            />
-          </div>
-
-          <IBtn
-            square
-            ghost
-            size="sm"
-            class="ml-3"
-            @click="removeItem(fertilizerData)"
-          >
-            <IconRemove :size="20" />
-          </IBtn>
-        </div>
-        <span
-          v-if="errors.fertilizersData"
-          class="text-xs text-error px-4 mt-1"
-        >{{ errors.fertilizersData }}</span>
-      </div>
-    </form>
+      :fertilizers="fertilizers"
+      @submit="saveAndNew"
+    />
 
     <template #actions>
       <IBtn
@@ -107,18 +52,16 @@ import type { Fertilizer, NewWateringSchema, NewWateringSchemaFertilizer } from 
 import {
   CirclePlus as IconAdd,
   MoveLeft as IconBack,
-  CircleMinus as IconRemove,
   Save as IconSave,
 } from 'lucide-vue-next'
-import { computed, inject } from 'vue'
+import { inject } from 'vue'
 import { useToast } from '../composables/useToast.ts'
 import { useWateringSchemaForm } from '../composables/useWateringSchemaForm.ts'
 import { REPO_WATERING_SCHEMA } from '../di_keys.ts'
-import { removeArrayElement } from '../util.ts'
 import IBtn from './IBtn.vue'
 import ICard from './ICard.vue'
 import ICardTitle from './ICardTitle.vue'
-import InputTextFloat from './InputTextFloat.vue'
+import WateringSchemaForm from './WateringSchemaForm.vue'
 
 interface Props {
   fertilizers: Array<Fertilizer>
@@ -134,15 +77,6 @@ const emit = defineEmits<Emits>()
 const wateringRepo = inject(REPO_WATERING_SCHEMA)
 const { toast } = useToast()
 
-const fallbackManufacturer = 'Unbekannter Hersteller'
-
-const sortedFertilizers = computed(
-  () => [...fertilizers].sort(
-    (lhs: Fertilizer, rhs: Fertilizer) =>
-      (lhs.manufacturer ?? fallbackManufacturer).localeCompare(rhs.manufacturer ?? fallbackManufacturer),
-  ),
-)
-
 const {
   name,
   fertilizersData,
@@ -151,32 +85,6 @@ const {
 } = useWateringSchemaForm({
   fertilizersData: [],
 })
-
-const unselectedFertilizers = computed(
-  (): Array<Fertilizer> => sortedFertilizers.value.filter(
-    item => fertilizersData.value.every(dataset => dataset.fertilizer.id !== item.id),
-  ),
-)
-
-function isSelected(fertilizer: Fertilizer): boolean {
-  return fertilizersData.value.some(item => item.fertilizer.id === fertilizer.id)
-}
-
-function addFertilizer() {
-  if (unselectedFertilizers.value.length === 0)
-    return
-
-  const dataset = {
-    fertilizer: unselectedFertilizers.value[0]!,
-    value: 5,
-  }
-
-  fertilizersData.value?.push(dataset)
-}
-
-function removeItem(item: FertilizerData) {
-  removeArrayElement(fertilizersData.value, i => i.fertilizer.id === item.fertilizer.id)
-}
 
 async function save() {
   if (!(await saveSchema()))
---
+++ src/components/WateringSchemaForm.vue
@@ -0,0 +1,138 @@
+<template>
+  <form
+    @submit.prevent="$emit('submit')"
+  >
+    <InputTextFloat
+      v-model="name"
+      :error="errorName"
+      label="Name des Zuchtschemas"
+      required
+    />
+
+    <hr class="my-3 text-base-200">
+
+    <div class="flex items-center justify-between mb-2">
+      <h3 class="text-xl font-semibold">
+        Enthaltene Dünger
+      </h3>
+
+      <IBtn variant="primary" circle class="text-base-100" @click="addFertilizer">
+        <IconAdd />
+      </IBtn>
+    </div>
+
+    <div class="space-y-3">
+      <div
+        v-for="(fertilizerData, i) in fertilizersData"
+        :key="i"
+        class="flex items-center"
+      >
+        <div class="flex-1 flex items-center justify-between">
+          <select v-model="fertilizerData.fertilizer" class="select">
+            <option
+              v-for="(fertilizer, j) in sortedFertilizers"
+              :key="j"
+              :value="fertilizer"
+              :disabled="isSelected(fertilizer)"
+            >
+              {{ fertilizer.manufacturer ?? fallbackManufacturer }} - {{ fertilizer.name }}
+            </option>
+          </select>
+
+          <InputTextFloat
+            v-model="fertilizerData.value"
+            label="ML pro Liter"
+            type="number"
+          />
+        </div>
+
+        <IBtn
+          square
+          ghost
+          size="sm"
+          class="ml-3"
+          @click="removeItem(fertilizerData)"
+        >
+          <IconRemove :size="20" />
+        </IBtn>
+      </div>
+      <span
+        v-if="errorFertilizerData"
+        class="text-xs text-error px-4 mt-1"
+      >{{ errorFertilizerData }}</span>
+    </div>
+  </form>
+</template>
+
+<script lang="ts" setup>
+import type { FertilizerData } from '../composables/useWateringSchemaForm.ts'
+import type { Fertilizer } from '../modules/nutrients/types'
+import {
+  CirclePlus as IconAdd,
+  CircleMinus as IconRemove,
+} from 'lucide-vue-next'
+import { computed } from 'vue'
+import { removeArrayElement } from '../util.ts'
+import IBtn from './IBtn.vue'
+import InputTextFloat from './InputTextFloat.vue'
+
+interface Props {
+  fertilizersData: Array<FertilizerData>
+  errorName?: string
+  errorFertilizerData?: string
+  fertilizers: Array<Fertilizer>
+}
+interface Emits {
+  'update:fertilizersData': [data: Array<FertilizerData>]
+  'submit': []
+}
+
+const { fertilizers, fertilizersData } = defineProps<Props>()
+const emit = defineEmits<Emits>()
+
+const name = defineModel<string>('name')
+
+const fallbackManufacturer = 'Unbekannter Hersteller'
+
+const sortedFertilizers = computed(
+  () => [...fertilizers].sort(
+    (lhs: Fertilizer, rhs: Fertilizer) =>
+      (lhs.manufacturer ?? fallbackManufacturer).localeCompare(rhs.manufacturer ?? fallbackManufacturer),
+  ),
+)
+
+const unselectedFertilizers = computed(
+  (): Array<Fertilizer> => sortedFertilizers.value.filter(
+    item => fertilizersData.every(dataset => dataset.fertilizer.id !== item.id),
+  ),
+)
+
+function isSelected(fertilizer: Fertilizer): boolean {
+  return fertilizersData.some(item => item.fertilizer.id === fertilizer.id)
+}
+
+function addFertilizer() {
+  console.info('add')
+  if (unselectedFertilizers.value.length === 0)
+    return
+
+  const dataset = {
+    fertilizer: unselectedFertilizers.value[0]!,
+    value: 5,
+  }
+
+  const data = [...fertilizersData]
+  data.push(dataset)
+
+  console.info('so', dataset, data)
+
+  emit('update:fertilizersData', data)
+}
+
+function removeItem(item: FertilizerData) {
+  const data = [...fertilizersData]
+  removeArrayElement(data, i => i.fertilizer.id === item.fertilizer.id)
+
+  emit('update:fertilizersData', data)
+}
+</script>
