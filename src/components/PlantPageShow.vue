79d03ea5c6b1ce0b53d9c8ca1e85b03ad1f0872f
Tobias S <tobias_schindler@hotmail.de>
2025-11-05 18:55:54 +0100

    [PlantPageShow] - scaffold navigation to add pour page

--- src/components/PlantPageShow.vue
+++ src/components/PlantPageShow.vue
@@ -1,98 +1,22 @@
 <template>
-  <ICard
-    v-if="plant"
-    class="w-full max-w-2xl"
-  >
-    <h1 class="text-4xl font-bold text-primary border-b border-b-base-200 mb-3">
-      {{ plantName }}
-    </h1>
-
-    <IStats class="shadow mt-2">
-      <IStat>
-        <IStatTitle>Substrat</IStatTitle>
-        <IStatValue>{{ plant.substrate.substrate }} ({{ plant.substrate.size }})</IStatValue>
-        <IStatDesc>{{ plant.substrate.info || 'Keine weiteren Informationen zum Substrat' }}</IStatDesc>
-      </IStat>
-
-      <IStat>
-        <IStatTitle>Startdatum</IStatTitle>
-        <IStatValue>{{ plantStartDate }}</IStatValue>
-        <IStatDesc>Der Grow ist am {{ plantStartDate }} gestartet worden</IStatDesc>
-      </IStat>
-    </IStats>
-
-    <div class="mt-2 grid grid-cols-1 sm:grid-cols-2">
-      <div>
-        <h2 class="text-xl font-semibold">
-          Timeline
-        </h2>
-        <ITimeline
-          vertical
-          :items="plantPhases"
-        >
-          <template #item="{ item, key }: { item: PhaseData, key: number }">
-            <ITimelineItem
-              box-start
-              :hide-start-line="key === 0"
-              :hide-end-line="key === (plantPhases.length - 1)"
-            >
-              <template #start>
-                {{ item.started }}
-              </template>
-
-              <template #middle>
-                <div class="border rounded-full p-1.5 flex items-center justify-center bg-neutral text-base-200">
-                  <component
-                    :is="item.icon"
-                    size="18"
-                  />
-                </div>
-              </template>
-
-              <template #end>
-                {{ item.label }}
-              </template>
-            </ITimelineItem>
-          </template>
-        </ITimeline>
-      </div>
-      <div />
-    </div>
-
-    <template #actions>
-      <IBtn>
-        <IconImage />
-        Bild hinzufügen
-      </IBtn>
-
-      <IBtn>
-        <IconWater />
-        Gießeintrag hinzufügen
-      </IBtn>
-    </template>
-  </ICard>
+  <PlantPageShowOverview
+    v-if="page === 'show'"
+    :plant="plant"
+    @add-pour="changePage('add-pour')"
+  />
+  <PlantPageShowAddPouring
+    v-else-if="page === 'add-pour'"
+    :plant="plant"
+    @back="back"
+  />
 </template>
 
 <script lang="ts" setup>
-import type { Component } from 'vue'
-import type { Plant, PlantPhase } from '../modules/plants/types'
-import {
-  Image as IconImage,
-  Droplet as IconWater,
-} from 'lucide-vue-next'
-import { formatDate } from 'sdx-php-date'
-import { computed } from 'vue'
-import { usePlantPhase } from '../composables/usePlantPhase.ts'
-import { now } from '../util.ts'
-import IBtn from './IBtn.vue'
-import ICard from './ICard.vue'
-import IStat from './IStat.vue'
-import IStatDesc from './IStatDesc.vue'
-import IStats from './IStats.vue'
-import IStatTitle from './IStatTitle.vue'
-import IStatValue from './IStatValue.vue'
-import ITimeline from './ITimeline.vue'
-import ITimelineItem from './ITimelineItem.vue'
+import type { Plant } from '../modules/plants/types'
+
+import { usePage } from '../composables/usePage.ts'
+import PlantPageShowAddPouring from './PlantPageShowAddPouring.vue'
+import PlantPageShowOverview from './PlantPageShowOverview.vue'
 
 interface Props {
   plant: Plant
@@ -104,40 +28,11 @@ interface Emits {
 const { plant } = defineProps<Props>()
 defineEmits<Emits>()
 
-type PhaseData = PlantPhase & { label: string, icon: Component, started: string }
+type Subpage = 'show' | 'add-pour'
 
-const { getPhaseIcon, getPhaseLabel } = usePlantPhase()
+const { page, changePage } = usePage<Subpage>('add-pour')
 
-const plantName = computed(() => {
-  const fallback = plant?.strain || 'Unknown'
-
-  if (plant?.name) {
-    return plant.name !== '' ? `${plant.name} (${fallback})` : fallback
-  }
-
-  return fallback
-})
-
-const plantPhases = computed(
-  (): Array<PhaseData> => plant?.phases
-    .map((phase: PlantPhase) => ({
-      ...phase,
-      label: getPhaseLabel(phase.phase),
-      icon: getPhaseIcon(phase.phase),
-      started: formatStartedAt(phase.startedAt),
-    }))
-    || [],
-)
-
-const plantStartDate = computed(() => {
-  const dateString = plant?.phases.find(p => p.phase === 'germination')?.startedAt || now()
-  const date = new Date(dateString)
-
-  return formatDate('d.m.Y', date)
-})
-
-function formatStartedAt(startedAt: string): string {
-  const date = new Date(startedAt)
-  return formatDate('d.m.Y', date)
+function back() {
+  changePage('show')
 }
 </script>
---
+++ src/components/PlantPageShowAddPouring.vue
@@ -0,0 +1,34 @@
+<template>
+  <ICard
+    class="w-full max-w-2xl"
+  >
+    add pour
+
+    <template #actions>
+      <IBtn
+        @click="$emit('back')"
+      >
+        <IconBack />
+        Back
+      </IBtn>
+    </template>
+  </ICard>
+</template>
+
+<script lang="ts" setup>
+import {
+  MoveLeft as IconBack,
+} from 'lucide-vue-next'
+import IBtn from './IBtn.vue'
+import ICard from './ICard.vue'
+
+interface Props {
+
+}
+interface Emits {
+  back: []
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
