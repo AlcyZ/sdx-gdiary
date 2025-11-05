425420cf5e7fc98ab64f8bc8d20b23c606527d02
Tobias S <tobias_schindler@hotmail.de>
2025-11-05 16:57:31 +0100

    [usePlantForm.ts] - composable for easy plant form integration

--- src/components/PlantPageAdd.vue
+++ src/components/PlantPageAdd.vue
@@ -50,17 +50,15 @@
 </template>
 
 <script lang="ts" setup>
-import type { NewPlant, NewPlantPhase, PlantPhaseType, PlantSubstrateType } from '../modules/plants/types'
+import type { NewPlant } from '../modules/plants/types'
 import type { Result, ToastVariant } from '../types'
-import { toTypedSchema } from '@vee-validate/yup'
 import {
   CirclePlus as IconAdd,
   MoveLeft as IconBack,
   Save as IconSave,
 } from 'lucide-vue-next'
-import { useForm } from 'vee-validate'
-import { computed, inject, ref } from 'vue'
-import * as yup from 'yup'
+import { inject, ref } from 'vue'
+import { usePlantForm } from '../composables/usePlantForm.ts'
 import { useToast } from '../composables/useToast.ts'
 import { REPO_PLANT } from '../di_keys.ts'
 import { err } from '../util.ts'
@@ -84,49 +82,21 @@ const { showToast } = useToast()
 
 const loading = ref(false)
 
-const ERR_STRAIN_REQUIRED = 'Die Sorte muss angegeben werden'
-const ERR_STRAIN_MAX = ({ max }: { max: number }) => `Die Sorte dar maximal ${max} Zeichen lang sein`
-
-const ERR_SUBSTRATE_REQUIRED = 'Es muss ein Substrat/Medium ausgewählt werden'
-const ERR_SUBSTRATE_SIZE_REQUIRED = 'Es muss eine Substratgröße angegeben werden'
-const ERR_PHASES_NOT_CHRONOLOGICALLY = 'Die Phasen müssen chronologisch aufeinander folgen'
-
-const phaseSchema = yup.object({
-  phase: yup.mixed<PlantPhaseType>().required(),
-  startedAt: yup.string().required('Asd muss'),
-  info: yup.string().optional(),
+const {
+  strain,
+  name,
+  substrate,
+  substrateSize,
+  phases,
+  validate,
+  errors,
+} = usePlantForm({
+  strain: '',
+  name: '',
+  substrate: '',
+  substrateSize: '',
+  phases: [],
 })
-
-const validationSchema = toTypedSchema(
-  yup.object({
-    strain: yup.string().required(ERR_STRAIN_REQUIRED).max(64, ERR_STRAIN_MAX),
-    name: yup.string().optional(),
-    substrate: yup.string().required(ERR_SUBSTRATE_REQUIRED),
-    substrateSize: yup.string().required(ERR_SUBSTRATE_SIZE_REQUIRED),
-    phases: yup.array()
-      .of(phaseSchema)
-      .test('is-ascending', ERR_PHASES_NOT_CHRONOLOGICALLY, isChronologicallySorted),
-  }),
-)
-
-const { validate, errors, defineField } = useForm({
-  validationSchema,
-  initialValues: {
-    strain: '',
-    name: '',
-    substrate: '',
-    substrateSize: '',
-    phases: [],
-  },
-})
-
-const hasFormErrors = computed(() => Object.keys(errors.value).length > 0)
-
-const [strain] = defineField('strain')
-const [name] = defineField('name')
-const [substrate] = defineField<'substrate', PlantSubstrateType>('substrate')
-const [substrateSize] = defineField<'substrateSize', string>('substrateSize')
-const [phases] = defineField<'phases', Array<NewPlantPhase>>('phases')
 
 function toast(message: string, variant: ToastVariant = 'error', close?: () => void) {
   showToast({
@@ -189,31 +159,5 @@ async function savePlant(): Promise<Result<undefined, string>> {
   loading.value = false
 
   return result.ok ? result : err('Pflanze konnte nicht gespeichert werden')
-}
-
-function isChronologicallySorted(phases: Array<NewPlantPhase> | undefined): boolean {
-  if (!phases || phases.length < 2) {
-    return true
-  }
-
-  for (let i = 0; i < phases.length - 1; i++) {
-    const current = phases[i]
-    const next = phases[i + 1]
-
-    if (!current?.startedAt || !next?.startedAt) {
-      continue
-    }
-
-    const currentDate = new Date(current.startedAt)
-    const nextDate = new Date(next.startedAt)
-
-    if (Number.isNaN(currentDate.getTime()) || Number.isNaN(nextDate.getTime()))
-      continue
-
-    if (currentDate > nextDate)
-      return false
-  }
-
-  return true
 }
 </script>
---
+++ src/composables/usePlantForm.ts
@@ -0,0 +1,82 @@
+import type { PartialDeep } from 'type-fest'
+import type { InferType } from 'yup'
+import type { NewPlantPhase, PlantPhaseType, PlantSubstrateType } from '../modules/plants/types'
+import { toTypedSchema } from '@vee-validate/yup'
+import { useForm } from 'vee-validate'
+import * as yup from 'yup'
+
+const ERR_STRAIN_REQUIRED = 'Die Sorte muss angegeben werden'
+const ERR_STRAIN_MAX = ({ max }: { max: number }) => `Die Sorte dar maximal ${max} Zeichen lang sein`
+
+const ERR_SUBSTRATE_REQUIRED = 'Es muss ein Substrat/Medium ausgewählt werden'
+const ERR_SUBSTRATE_SIZE_REQUIRED = 'Es muss eine Substratgröße angegeben werden'
+const ERR_PHASES_NOT_CHRONOLOGICALLY = 'Die Phasen müssen chronologisch aufeinander folgen'
+
+const phaseSchema = yup.object({
+  phase: yup.mixed<PlantPhaseType>().required(),
+  startedAt: yup.string().required('Asd muss'),
+  info: yup.string().optional(),
+})
+
+const plantSchema = yup.object({
+  strain: yup.string().required(ERR_STRAIN_REQUIRED).max(64, ERR_STRAIN_MAX),
+  name: yup.string().optional(),
+  substrate: yup.string().required(ERR_SUBSTRATE_REQUIRED),
+  substrateSize: yup.string().required(ERR_SUBSTRATE_SIZE_REQUIRED),
+  phases: yup.array()
+    .of(phaseSchema)
+    .test('is-ascending', ERR_PHASES_NOT_CHRONOLOGICALLY, isChronologicallySorted),
+})
+
+const validationSchema = toTypedSchema(plantSchema)
+
+export type PlantForm = InferType<typeof plantSchema>
+
+function isChronologicallySorted(phases: Array<NewPlantPhase> | undefined): boolean {
+  if (!phases || phases.length < 2) {
+    return true
+  }
+
+  for (let i = 0; i < phases.length - 1; i++) {
+    const current = phases[i]
+    const next = phases[i + 1]
+
+    if (!current?.startedAt || !next?.startedAt) {
+      continue
+    }
+
+    const currentDate = new Date(current.startedAt)
+    const nextDate = new Date(next.startedAt)
+
+    if (Number.isNaN(currentDate.getTime()) || Number.isNaN(nextDate.getTime()))
+      continue
+
+    if (currentDate > nextDate)
+      return false
+  }
+
+  return true
+}
+
+export function usePlantForm(initialValues?: PartialDeep<PlantForm>) {
+  const { validate, errors, defineField } = useForm({
+    validationSchema,
+    initialValues,
+  })
+
+  const [strain] = defineField('strain')
+  const [name] = defineField('name')
+  const [substrate] = defineField<'substrate', PlantSubstrateType>('substrate')
+  const [substrateSize] = defineField<'substrateSize', string>('substrateSize')
+  const [phases] = defineField<'phases', Array<NewPlantPhase>>('phases')
+
+  return {
+    strain,
+    name,
+    substrate,
+    substrateSize,
+    phases,
+    validate,
+    errors,
+  }
+}
