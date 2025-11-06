62bf1395d26ca87dee67dea0cd1a920a0de63aaf
Tobias S <tobias_schindler@hotmail.de>
2025-11-06 15:43:07 +0100

    [IInputNumber.vue] - component for number input fields

---
+++ src/components/IInputNumber.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <label class="floating-label">
+      <input
+        v-model.number="model"
+        type="number"
+        class="input"
+        :class="inputClass"
+      >
+      <span>{{ label }}</span>
+    </label>
+    <div
+      v-if="error"
+      class="px-4 text-xs text-error flex items-center"
+    >
+      <XIcon
+        :size="16"
+      />
+      <span>{{ error }}</span>
+    </div>
+    <div
+      v-if="info"
+      class="px-4 text-xs opacity-60"
+    >
+      {{ info }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { XIcon } from 'lucide-vue-next'
+import { computed } from 'vue'
+
+interface Props {
+  label: string
+  error?: string
+  info?: string
+  fullWidth?: boolean
+}
+interface Emits {
+
+}
+
+const { fullWidth } = defineProps<Props>()
+defineEmits<Emits>()
+
+const model = defineModel<number>()
+
+const inputClass = computed(() => fullWidth ? 'w-full' : undefined)
+</script>
