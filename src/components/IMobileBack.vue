c4c50de5938742448c515feb80670be350cdb5fa
Tobias S <tobias_schindler@hotmail.de>
2025-11-06 21:13:20 +0100

    [IMobileBack.vue] for mobile back navigation

---
+++ src/components/IMobileBack.vue
@@ -0,0 +1,25 @@
+<template>
+  <IBtn
+    ghost
+    size="lg"
+    class="fixed top-5 left-0 z-50 block sm:hidden"
+    @click="$emit('back')"
+  >
+    <ChevronLeft :size="32" />
+  </IBtn>
+</template>
+
+<script lang="ts" setup>
+import { ChevronLeft } from 'lucide-vue-next'
+import IBtn from './IBtn.vue'
+
+interface Props {
+
+}
+interface Emits {
+  back: []
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
