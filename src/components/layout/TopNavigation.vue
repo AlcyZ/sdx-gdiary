7758f6fe4c2fed6b3aaa1cebe49d9244849eb774
Tobias S <tobias_schindler@hotmail.de>
2025-11-11 13:45:00 +0100

    [chore] - TopNavigation layout component

---
+++ src/components/layout/TopNavigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="bg-[#16a34a] flex items-center justify-between">
+    <IBtn
+      ghost
+      square
+      variant="neutral"
+      class="rounded-none text-base-100"
+      @click="$emit('back')"
+    >
+      <IconBack />
+    </IBtn>
+
+    <span
+      v-if="title"
+      class="text-base-100 font-semibold absolute left-1/2 -translate-x-1/2"
+    >{{ title }}</span>
+
+    <IDropdown
+      v-if="actions"
+      :items="actions"
+      btn-class="rounded-none text-base-100"
+      position="end"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { TopNavigationAction } from '../../types'
+import {
+  ChevronLeft as IconBack,
+} from 'lucide-vue-next'
+import IBtn from '../ui/IBtn.vue'
+import IDropdown from '../ui/IDropdown.vue'
+
+interface Props {
+  title?: string
+  actions?: Array<TopNavigationAction>
+}
+interface Emits {
+  back: []
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
