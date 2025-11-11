80705dfbfbfe2a612235c2b150c71f4ea78cb416
Tobias S <tobias_schindler@hotmail.de>
2025-11-11 17:30:12 +0100

    [ICarousel.vue] - carousel component for images

---
+++ src/components/ui/ICarousel.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="carousel" :class="carouselClass">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+interface Props {
+  position?: CarouselPosition | undefined
+  vertical?: boolean
+}
+interface Emits {
+
+}
+
+type CarouselPosition = 'start' | 'center' | 'end'
+
+const { position, vertical = false } = defineProps<Props>()
+defineEmits<Emits>()
+
+const carouselPositionMap: Record<CarouselPosition, string> = {
+  start: 'carousel-start',
+  center: 'carousel-center',
+  end: 'carousel-end',
+}
+
+const carouselClass = computed(() => [
+  position ? carouselPositionMap[position] : undefined,
+  vertical ? 'carousel-vertical' : 'carousel-horizontal',
+])
+</script>
