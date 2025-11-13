f528d96c40c066e2030a32a93f3731cef6662153
Tobias S <tobias_schindler@hotmail.de>
2025-11-13 23:57:16 +0100

    [task] - IImgAsync.vue utility component for markup of async rendered images

---
+++ src/components/ui/IImgAsync.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="w-full h-full inline-block relative overflow-hidden bg-[#E0E0E0]"
+  >
+    <div
+      v-if="loading"
+      class="absolute top-0 left-0 w-full h-full flex items-center justify-center z-10 bg-white/50"
+    >
+      <ILoading
+        ring
+      />
+    </div>
+
+    <img
+      :src="imgSrc"
+      alt=""
+      class="w-full h-full object-cover"
+      loading="lazy"
+    >
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ILoading from './ILoading.vue'
+
+interface Props {
+  imgSrc: string
+  loading: boolean
+}
+interface Emits {
+
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
