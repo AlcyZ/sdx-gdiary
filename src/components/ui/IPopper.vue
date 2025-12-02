93488e8794d10626c7d5ab65ac3cd3a194e6c9de
Tobias S <tobias_schindler@hotmail.de>
2025-12-02 15:51:15 +0100

    [task] - IPopper and IPopperSelf components for simple popovers

---
+++ src/components/ui/IPopper.vue
@@ -0,0 +1,22 @@
+<template>
+  <Popper v-bind="$attrs" hover open-delay="200" close-delay="100">
+    <slot />
+    <template #content="props">
+      <slot name="content" v-bind="props" />
+    </template>
+  </Popper>
+</template>
+
+<script lang="ts" setup>
+import Popper from 'vue3-popper'
+
+interface Props {
+
+}
+interface Emits {
+
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
---
+++ src/components/ui/IPopperSelf.vue
@@ -0,0 +1,26 @@
+<template>
+  <IPopper>
+    <slot name="default" />
+
+    <template #content>
+      <slot name="default" />
+    </template>
+  </IPopper>
+</template>
+
+<script lang="ts" setup>
+import IPopper from './IPopper.vue'
+
+interface Props {
+
+}
+interface Emits {
+
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+defineSlots<{
+  default: (props: Record<string, never>) => any
+}>()
+</script>
