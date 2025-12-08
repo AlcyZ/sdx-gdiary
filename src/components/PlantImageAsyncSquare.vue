63593e594428f83c6e47e0395d5e519cc97d069f
Tobias S <tobias_schindler@hotmail.de>
2025-12-08 14:47:16 +0100

    [refactoring] - refactored plant image components to use central component for image src loading

--- src/components/PlantImageAsyncSquare.vue
+++ src/components/PlantImageAsyncSquare.vue
@@ -1,26 +1,30 @@
 <template>
-  <div
-    class="
+  <PlantImgAsync
+    :image
+  >
+    <template #default="{ src }">
+      <div
+        class="
       w-full
       aspect-square
       overflow-hidden
       shadow-md
       bg-gray-200
     "
-  >
-    <img
-      :src="imgSrc"
-      :alt="`plant-image-${image.id}`"
-      class="w-full h-full object-cover"
-    >
-  </div>
+      >
+        <img
+          :src="src"
+          :alt="`plant-image-${image.id}`"
+          class="w-full h-full object-cover"
+        >
+      </div>
+    </template>
+  </PlantImgAsync>
 </template>
 
 <script lang="ts" setup>
 import type { PlantImage } from '../modules/plants/types'
-import { inject, ref, watch } from 'vue'
-import { REPO_PLANT } from '../di_keys.ts'
-import { BASE64_PLACEHOLDER, none } from '../util.ts'
+import PlantImgAsync from './PlantImgAsync.vue'
 
 interface Props {
   image: PlantImage
@@ -31,26 +35,4 @@ interface Emits {
 
 const { image } = defineProps<Props>()
 defineEmits<Emits>()
-
-const plantRepo = inject(REPO_PLANT)
-
-const imgSrc = ref(BASE64_PLACEHOLDER)
-
-const isLoading = ref(false)
-
-watch(() => image, newImage => loadImage(newImage), { immediate: true })
-
-async function loadImage(plantImage: PlantImage) {
-  isLoading.value = true
-  const imageOption = await plantRepo?.getImageByImageId(plantImage.id) || none()
-
-  isLoading.value = false
-  if (imageOption.exist) {
-    const blob = new Blob([imageOption.value.data], { type: imageOption.value.mime })
-    imgSrc.value = URL.createObjectURL(blob)
-  }
-  else {
-    imgSrc.value = BASE64_PLACEHOLDER
-  }
-}
 </script>
---
+++ src/components/PlantImgAsync.vue
@@ -0,0 +1,51 @@
+<template>
+  <slot name="default" :src="imgSrc" :is-loading="isLoading" />
+</template>
+
+<script lang="ts" setup>
+import type { PlantImage } from '../modules/plants/types'
+import { inject, onMounted, ref } from 'vue'
+import { REPO_PLANT } from '../di_keys.ts'
+import { BASE64_PLACEHOLDER, none } from '../util.ts'
+
+interface Props {
+  image: PlantImage
+}
+interface Emits {
+
+}
+
+const { image } = defineProps<Props>()
+defineEmits<Emits>()
+defineSlots<{
+  default: (props: { src: string, isLoading: boolean }) => any
+}>()
+
+const plantRepo = inject(REPO_PLANT)
+
+const imgSrc = ref(BASE64_PLACEHOLDER)
+
+const isLoading = ref(false)
+
+function waitFor(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+async function loadImage() {
+  isLoading.value = true
+  const imageOption = await plantRepo?.getImageByImageId(image.id) || none()
+
+  await waitFor(500)
+
+  isLoading.value = false
+  if (imageOption.exist) {
+    const blob = new Blob([imageOption.value.data], { type: imageOption.value.mime })
+    imgSrc.value = URL.createObjectURL(blob)
+  }
+  else {
+    imgSrc.value = BASE64_PLACEHOLDER
+  }
+}
+
+onMounted(loadImage)
+</script>
