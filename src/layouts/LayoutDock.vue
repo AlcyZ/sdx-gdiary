f00094828d4649e7c37ade0c9de6045f1a4fbaaa
Tobias S <tobias_schindler@hotmail.de>
2025-12-12 18:47:38 +0100

    [refactoring] - add theme styles

--- src/layouts/LayoutDock.vue
+++ src/layouts/LayoutDock.vue
@@ -11,9 +11,23 @@
         class="flex-1 py-sm"
         :to="dock.to"
       >
-        <div class="flex flex-col items-center justify-center">
+        <div class="flex flex-col items-center justify-center relative">
           <component :is="dock.icon" />
           <span class="text-xs">{{ dock.label }}</span>
+          <transition
+            appear
+            enter-active-class="transition-all duration-300 ease-out"
+            enter-from-class="opacity-0 scale-x-0"
+            enter-to-class="opacity-100 scale-x-100"
+            leave-active-class="transition-all duration-200 ease-in"
+            leave-from-class="opacity-100 scale-x-100"
+            leave-to-class="opacity-0 scale-x-0"
+          >
+            <div
+              v-if="$route.path.startsWith(dock.to)"
+              class="h-[3px] w-2/5 bg-dim rounded-full absolute -bottom-1"
+            />
+          </transition>
         </div>
       </RouterLink>
     </div>
