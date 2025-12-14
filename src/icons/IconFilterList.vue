b42523629a04f4e4d6a8cc6ac0684ef348e6e25b
Tobias S <tobias_schindler@hotmail.de>
2025-12-14 21:53:38 +0100

    [task] - refactored plant listing card view

---
+++ src/icons/IconFilterList.vue
@@ -0,0 +1,27 @@
+
+<template>
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    :height="sizePx"
+    viewBox="0 -960 960 960"
+    :width="sizePx"
+    :fill="fill">
+    <path d="M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z"/></svg>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+interface Props {
+  size?: number
+  fill?: string
+}
+interface Emits {
+
+}
+
+const { size = 24, fill = '#030303' } = defineProps<Props>()
+defineEmits<Emits>()
+
+const sizePx = computed(() => `${size}px`)
+</script>
