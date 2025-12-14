54ce8b23c3b42bf4d3819d41fbcfa95b04b2e9e5
Tobias S <tobias_schindler@hotmail.de>
2025-12-14 10:09:42 +0100

    [refactoring] - PlantListing.vue testing code

--- src/views/PlantListing.vue
+++ src/views/PlantListing.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
-    plant listing
+    <div
+      v-for="(plant, i) in plantStore.plants"
+      :key="i"
+    >
+      {{ plant.strain }}
+      -
+      <RouterLink :to="{ name: ROUTE_PLANT_DETAILS, params: { plantId: plant.id } }">
+        Details
+      </RouterLink>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { useLayout } from '../composables/useLayout.ts'
+import { ROUTE_PLANT_DETAILS } from '../routes.ts'
+import { usePlantStore } from '../stores/plantStore.ts'
 
 interface Props {
 
@@ -16,6 +27,8 @@ interface Emits {
 
 defineProps<Props>()
 defineEmits<Emits>()
+
+const plantStore = usePlantStore()
 
 useLayout({
   dockVisible: true,
