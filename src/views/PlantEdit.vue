f75ef6948f9ae59dfa61ab04cf2da2ca96deb0d7
Tobias S <tobias_schindler@hotmail.de>
2025-12-14 20:09:19 +0100

    [task] - scaffold views

---
+++ src/views/PlantEdit.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    plant edit
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useLayout } from '../composables/useLayout.ts'
+
+interface Props {
+
+}
+interface Emits {
+
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+
+useLayout({
+  dockVisible: true,
+  topNavigationVisible: false,
+})
+</script>
---
+++ src/views/PlantListing.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div
+      v-for="(plant, i) in plantStore.plants"
+      :key="i"
+    >
+      {{ plant.strain }}
+      -
+      <RouterLink :to="{ name: ROUTE_PLANT_DETAILS, params: { plantId: plant.id } }">
+        Details
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useLayout } from '../composables/useLayout.ts'
+import { ROUTE_PLANT_DETAILS } from '../routes.ts'
+import { usePlantStore } from '../stores/plantStore.ts'
+
+interface Props {
+
+}
+interface Emits {
+
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+
+const plantStore = usePlantStore()
+
+useLayout({
+  dockVisible: true,
+  topNavigationVisible: false,
+})
+</script>
