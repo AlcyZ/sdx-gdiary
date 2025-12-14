31498aea7eeda5fa604a05a2e36ccdd4a7c867f1
Tobias S <tobias_schindler@hotmail.de>
2025-12-14 16:32:31 +0100

    [task] - new dock navigation component

---
+++ src/components/layout/NavigationDock.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="shrink-0 flex border border-subtle bg-white">
+    <NavigationDockItem
+      v-for="(dock, i) in docks"
+      :key="i"
+      :dock
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { DockItem } from '../../types'
+import { markRaw, ref } from 'vue'
+import IconGalleryThumbnail from '../../icons/IconGalleryThumbnail.vue'
+import IconLabResearch from '../../icons/IconLabResearch.vue'
+import IconPottedPlant from '../../icons/IconPottedPlant.vue'
+import IconSettings from '../../icons/IconSettings.vue'
+import NavigationDockItem from './NavigationDockItem.vue'
+
+interface Props {
+
+}
+interface Emits {
+
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+
+const docks = ref<Array<DockItem>>([
+  {
+    label: 'Pflanzen',
+    icon: markRaw(IconPottedPlant),
+    to: '/plants',
+  },
+  {
+    label: 'Dünger',
+    icon: markRaw(IconLabResearch),
+    to: '/nutrients',
+  },
+  {
+    label: 'Galerie',
+    icon: markRaw(IconGalleryThumbnail),
+    to: '/gallery',
+  },
+  {
+    label: 'Einstellungen',
+    icon: markRaw(IconSettings),
+    to: '/settings',
+  },
+])
+</script>
---
+++ src/components/layout/NavigationDockItem.vue
@@ -0,0 +1,39 @@
+<template>
+  <RouterLink
+    class="flex-1 py-sm"
+    :to="dock.to"
+  >
+    <div class="flex flex-col items-center justify-center relative">
+      <component :is="dock.icon" />
+      <span class="text-xs">{{ dock.label }}</span>
+      <transition
+        appear
+        enter-active-class="transition-all duration-300 ease-out"
+        enter-from-class="opacity-0 scale-x-0"
+        enter-to-class="opacity-100 scale-x-100"
+        leave-active-class="transition-all duration-200 ease-in"
+        leave-from-class="opacity-100 scale-x-100"
+        leave-to-class="opacity-0 scale-x-0"
+      >
+        <div
+          v-if="$route.path.startsWith(dock.to)"
+          class="h-[3px] w-2/5 bg-dim rounded-full absolute -bottom-1"
+        />
+      </transition>
+    </div>
+  </RouterLink>
+</template>
+
+<script lang="ts" setup>
+import type { DockItem } from '../../types'
+
+interface Props {
+  dock: DockItem
+}
+interface Emits {
+
+}
+
+defineProps<Props>()
+defineEmits<Emits>()
+</script>
