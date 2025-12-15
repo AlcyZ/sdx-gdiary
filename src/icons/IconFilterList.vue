2cd2d31a13493554f010fa2294bd5153d45a1acb
Tobias S <tobias_schindler@hotmail.de>
2025-12-15 10:25:51 +0100

    [task] - add material icon filter list

--- src/icons/IconFilterList.vue
+++ src/icons/IconFilterList.vue
@@ -1,12 +1,12 @@
-
 <template>
-<svg
+  <svg
     xmlns="http://www.w3.org/2000/svg"
     :height="sizePx"
     viewBox="0 -960 960 960"
     :width="sizePx"
-    :fill="fill">
-    <path d="M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z"/></svg>
+    :fill="fill"
+  >
+    <path d="M400-240v-80h160v80H400ZM240-440v-80h480v80H240ZM120-640v-80h720v80H120Z" /></svg>
 </template>
 
 <script lang="ts" setup>
