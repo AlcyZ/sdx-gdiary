5143ef7b1930333822a6f033159ed1ad56d63e28
Tobias S <tobias_schindler@hotmail.de>
2025-12-17 17:48:23 +0100

    [task] - add animations to settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,28 +1,60 @@
 <template>
-  <div class="flex flex-col gap-3 items-center justify-center flex-1 my-4">
-    <SettingsDataExport />
-
-    <SettingsDataImport />
-
-    <SettingsUpdate />
-
-    <div class="divider text-gray-900/30">
-      Entwickler Tools
-    </div>
-
-    <SettingsSeed class="opacity-75" />
-
-    <SettingsDebug class="opacity-75" />
-  </div>
+  <motion.ul
+    class="flex flex-col gap-3 items-center justify-center flex-1 my-4 p-4"
+    :variants="fadeLeft.list"
+    initial="from"
+    animate="to"
+  >
+    <motion.li
+      class="w-full"
+      :variants="fadeLeft.item"
+    >
+      <SettingsDataExport />
+    </motion.li>
+    <motion.li
+      class="w-full"
+      :variants="fadeLeft.item"
+    >
+      <SettingsDataImport />
+    </motion.li>
+    <motion.li
+      class="w-full"
+      :variants="fadeLeft.item"
+    >
+      <SettingsUpdate />
+    </motion.li>
+    <motion.li
+      class="w-full"
+      :variants="fadeLeft.item"
+    >
+      <div class="divider text-gray-900/30">
+        Entwickler Tools
+      </div>
+    </motion.li>
+    <motion.li
+      class="w-full"
+      :variants="fadeLeft.item"
+    >
+      <SettingsSeed class="opacity-75" />
+    </motion.li>
+    <motion.li
+      class="w-full"
+      :variants="fadeLeft.item"
+    >
+      <SettingsDebug class="opacity-75" />
+    </motion.li>
+  </motion.ul>
 </template>
 
 <script lang="ts" setup>
+import { motion } from 'motion-v'
 import SettingsDataExport from '../components/SettingsDataExport.vue'
 import SettingsDataImport from '../components/SettingsDataImport.vue'
 import SettingsDebug from '../components/SettingsDebug.vue'
 import SettingsSeed from '../components/SettingsSeed.vue'
 import SettingsUpdate from '../components/SettingsUpdate.vue'
 import { usePageLayout } from '../composables/usePageLayout.ts'
+import useStaggerAnimation from '../composables/useStaggerAnimation.ts'
 
 interface Props {
 
@@ -37,4 +69,5 @@ defineEmits<Emits>()
 usePageLayout({
   dock: true,
 })
+const { fadeLeft } = useStaggerAnimation()
 </script>
