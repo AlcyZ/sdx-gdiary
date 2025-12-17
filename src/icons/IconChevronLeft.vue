db9e01c4df724e6cf2590d515be34e521df9742f
Tobias S <tobias_schindler@hotmail.de>
2025-12-17 14:52:09 +0100

    [task] - improved icons

--- src/icons/IconChevronLeft.vue
+++ src/icons/IconChevronLeft.vue
@@ -1,12 +1,12 @@
-
 <template>
-<svg
+  <svg
     xmlns="http://www.w3.org/2000/svg"
     :height="sizePx"
     viewBox="0 -960 960 960"
     :width="sizePx"
-    :fill="fill">
-    <path d="M560-221.91 301.91-480 560-738.09 634.09-664l-184 184 184 184L560-221.91Z"/></svg>
+    :fill="fill"
+  >
+    <path d="M560-221.91 301.91-480 560-738.09 634.09-664l-184 184 184 184L560-221.91Z" /></svg>
 </template>
 
 <script lang="ts" setup>
